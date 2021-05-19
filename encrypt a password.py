81e83aebe252ec954d26a632eeb09717c7d04f0e
manisha069 <55616388+manisha069@users.noreply.github.com>
2021-05-20 01:46:12 +0530

    Update encrypt a password.py

--- encrypt a password.py
+++ encrypt a password.py
@@ -3,5 +3,5 @@ new_password=""
 for i in password:
     x=ord(i)//2
     y=ord(i)%2
-    new_password=new_password+str(x)+str(y)+"."
+    new_password+=+str(x)+str(y)+"."
 print("your encrypted password is: ", new_password)
